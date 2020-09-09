313b507996901e2c7a32b2a5093ce215a88323c9
Sofia <medi61200@mail.ru>
2020-09-09 23:31:17 +0300

    Gulp, link

--- gulpfile.js
+++ gulpfile.js
@@ -6,33 +6,32 @@ const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const csso = require("gulp-csso");
 const rename = require("gulp-rename");
+const sync = require("browser-sync").create();
 
 // Styles
 
 const styles = () => {
   return gulp.src("source/sass/style.scss")
-  .pipe(plumber())
-  .pipe(sourcemap.init())
-  .pipe(sass())
-  .pipe(postcss([
-    autoprefixer()
-  ]))
-  .pipe(csso())
-  .pipe(rename("styles.min.css"))
-  .pipe(sourcemap.write("."))
-  .pipe(gulp.dest("source/css"))
- }
-
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(sync.stream());
+}
 exports.styles = styles;
 
 // Server
 
-const sync = require("browser-sync").create();
-
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -47,49 +46,49 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", { delay: 500 }, gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/*.html", { delay: 500 }, gulp.series("copyHtml"));
+  gulp.watch("build/*.html").on("change", sync.reload);
 }
-
-exports.default = gulp.series(
-  styles, server, watcher
-);
 
 // Clean
 
- const del = require("del");
- const clean = () => {
+const del = require("del");
+const clean = () => {
   return del("build");
- };
- exports.clean = clean;
+};
+exports.clean = clean;
 
 // Copy
 
 const copy = () => {
   return gulp.src([
-  "source/fonts/**/*.{woff,woff2}",
-  "source/img/**",
-  "source/js/**",
-  "source/css/**",
-  "source/*.ico",
-  "source/*.html"
-  ], {
-  base: "source"
-  })
-  .pipe(gulp.dest("build"));
+    "source/fonts/**/*.{woff,woff2}",
+    "source/js/**",
+    "source/*.ico"
+  ])
+    .pipe(gulp.dest("build"));
 };
 exports.copy = copy;
 
+const copyHtml = () => {
+  return gulp.src([
+    "source/*.html"
+  ])
+    .pipe(gulp.dest("build"));
+};
+exports.copyHtml = copyHtml;
 // Images
 
 const imagemin = require("gulp-imagemin");
 const images = () => {
- return gulp.src("source/img/**/*.{jpg,png,svg}")
-  .pipe(imagemin([
-    imagemin.optipng({quality: 75,optimizationLevel: 3}),
-    imagemin.mozjpeg({progressive: true}),
-    imagemin.svgo()
-  ]))
-}
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ quality: 75, optimizationLevel: 3 }),
+      imagemin.mozjpeg({ progressive: true }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+};
 exports.images = images;
 
 // Sprite
@@ -97,13 +96,13 @@ exports.images = images;
 const svgstore = require("gulp-svgstore");
 const sprite = () => {
   return gulp.src([
-    "source/img/**/icon-*.svg",
-    "source/img/**/logo-htmlacademy.svg",
-    "source/img/**/logo-footer.svg"
+    "build/img/**/icon-*.svg",
+    "build/img/**/logo-htmlacademy.svg",
+    "build/img/**/logo-footer.svg"
   ])
-  .pipe(svgstore())
-  .pipe(rename("sprite.svg"))
-  .pipe(gulp.dest("source/img"))
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
 }
 exports.sprite = sprite;
 
@@ -111,9 +110,9 @@ exports.sprite = sprite;
 
 const webp = require("gulp-webp");
 const createWebp = () => {
- return gulp.src("source/img/**/*.{png,jpg}")
- .pipe(webp({quality: 90}))
- .pipe(gulp.dest("source/img"))
+  return gulp.src("build/img/**/*.{png,jpg}")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("build/img"))
 }
 exports.webp = createWebp;
 
@@ -125,6 +124,11 @@ const build = gulp.series(
   images,
   sprite,
   createWebp,
-  copy
- );
- exports.build = build
+  copy,
+  copyHtml
+);
+exports.build = build
+
+exports.default = gulp.series(
+  build, server, watcher
+);
